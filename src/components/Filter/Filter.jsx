d32981d5b8e62cc9b9e72983faf32e641eb134ef
Andrey F <media.andrej@gmail.com>
2022-02-06 19:32:13 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,7 +5,6 @@ import { getFilter } from '../../redux/contacts/contacts-selectors';
 import Context from '../../contexts/context';
 import s from './Filter.module.css';
 
-
 export default function Filter() {
   const { filter, setFilter } = useContext(Context);
   const onChange = (e) => setFilter(e.currentTarget.value);
