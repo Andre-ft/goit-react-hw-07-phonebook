c907d2a295f005b00f2bba12d79127595f278013
Andrey F <media.andrej@gmail.com>
2022-02-06 19:28:38 +0300

    Update Provider.js

--- src/contexts/Provider.js
+++ src/contexts/Provider.js
@@ -3,18 +3,7 @@ import Context from './context';
 
 export default function Provider({ children }) {
   const [filter, setFilter] = useState('');
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // const onLogIn = () => {
-  //   setUser({ name: 'Манго', avatar });
-  //   setIsLoggedIn(true);
-  // };
-
-  // const onLogOut = () => {
-  //   setUser(null);
-  //   setIsLoggedIn(false);
-  // };
-
+  
   const providerValue = useMemo(() => {
     return { filter, setFilter };
   }, [setFilter, filter]);
